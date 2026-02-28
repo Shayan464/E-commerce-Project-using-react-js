723a1487e86c3410d52ecde333a626d09a70118f
shayan <shayan8839@gmail.com>
2026-02-28 23:28:12 +0530

    ui updated

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,37 +1,39 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
-    const saved = localStorage.getItem("userLoggedIn");
+    const saved = localStorage.getItem('userLoggedIn');
     return saved ? JSON.parse(saved) : null;
   });
-  
 
   useEffect(() => {
     if (user) {
-      localStorage.setItem("userLoggedIn", JSON.stringify(user));
+      localStorage.setItem('userLoggedIn', JSON.stringify(user));
     } else {
-      localStorage.removeItem("userLoggedIn");
+      localStorage.removeItem('userLoggedIn');
     }
   }, [user]);
-  
-  
+
   const login = (Email, Password) => {
-    const storedUsers = JSON.parse(localStorage.getItem("userData")) || [];
-    const foundUser = storedUsers.find(u => u.Email === Email && u.Password === Password);
+    const storedUsers = JSON.parse(localStorage.getItem('userData')) || [];
+    const foundUser = storedUsers.find(
+      (u) =>
+        u.Email.toLowerCase() === Email.toLowerCase() && u.Password === Password
+    );
+
     if (foundUser) {
       setUser(foundUser);
-    
+
       return { success: true, user: foundUser };
-    };
+    }
     return { success: false };
   };
-  
-    const logout = () => {
-      setUser(null);
-    };
+
+  const logout = () => {
+    setUser(null);
+  };
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
